8d0859cbd9442bbcc9efb40640ace96fac04a731
akifnihat <akifst@code.edu.az>
2024-02-14 14:20:38 +0400

    First commit

---
+++ c#-Task/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿//Console.WriteLine("Hello, World!");
+
+#region task1
+//int num = 14;
+//if (num % 2 == 0)
+//{
+//    Console.WriteLine("cut");
+//}
+//else
+//{
+//    Console.WriteLine("tek");
+//}
+#endregion
+#region task2
+//int a = 25;
+//if (a % 3 == 0 && a% 7 == 0)
+//{
+//    Console.WriteLine("bolunur")
+//}
+//else
+//{
+//    Console.WriteLine("bolunmur")
+//}
+#endregion
+#region task3
+//int x = 12;
+//int y = 15;
+//if (x % 2 == 0 && y % 2 == 0)
+//{
+//    Console.WriteLine(x+y)
+//}
+//else
+//{
+//    Console.WrriteLine(x-y)
+//}
+#endregion
+#region task4
+//int temp = 17;
+//if (temp>15)
+//{
+//    Console.WriteLine("isti")
+//}
+//else
+//{
+//    Console.WriteLine("soyuqdur")
+//}
+#endregion
